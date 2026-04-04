d30ceb519d9d8a9e3dc91046320c67c72019c9bf
rand-diaa <130349080+rand-diaa@users.noreply.github.com>
2026-04-04 22:28:40 +0300

    Delete student directory

--- student/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Student
-
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "email")
-    search_fields = ("user__username", "user__first_name", "user__last_name")
